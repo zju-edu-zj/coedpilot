6e3d461d42adc8d2e8e66490afc3c4c318cec15f
zju-edu-zj <2943168611@qq.com>
2025-02-12 21:19:01 +0800

    add dataset

--- line_locator/run.sh
+++ line_locator/run.sh
@@ -1,9 +1,9 @@
-lang=typescript
+lang=python
 lr=5e-5
 batch_size=20
 source_length=512
 target_length=512
-data_dir=dataset
+data_dir=old_dataset
 output_dir=model/$lang
 train_file=$data_dir/$lang/train.jsonl
 dev_file=$data_dir/$lang/dev.jsonl
@@ -12,7 +12,7 @@ epochs=2
 pretrained_model=microsoft/codebert-base
 beam_size=10
 
-CUDA_VISIBLE_DEVICES=0,1 nohup python run.py --do_train --do_eval --do_test --model_type roberta --model_name_or_path $pretrained_model \
+CUDA_VISIBLE_DEVICES=0,1 python run.py --do_train --do_eval --do_test --model_type roberta --model_name_or_path $pretrained_model \
 --train_filename $train_file --dev_filename $dev_file --test_filename $test_file --output_dir $output_dir --max_source_length $source_length \
 --max_target_length $target_length --beam_size $beam_size --train_batch_size $batch_size --eval_batch_size $batch_size \
---learning_rate $lr --num_train_epochs $epochs &
+--learning_rate $lr --num_train_epochs $epochs  | tee output.log
